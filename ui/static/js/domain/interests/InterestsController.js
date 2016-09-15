8fc164b5f59681181a59b5e8d5ee02edf4f2fd1e
mzakov <m.zakov@gmail.com>
2016-09-15 17:00:32 -0500

    .

--- ui/static/js/domain/interests/InterestsController.js
+++
@@ -1,9 +0,0 @@
-angular.module('app').controller('InterestsController', ['InterestsService', 'allInterests', function InterestsController(InterestsService, allInterests) {
-
-	var ctrl = this;
-	
-	ctrl.interests = allInterests.data;
-	
-	console.dir(ctrl.interests);
-	
-}]);
--- ui/static/js/domain/interests/InterestsService.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').service('InterestsService', ['$http', 'srvUrl', function($http, srvUrl){
-
-	var url = 'interests/';
-	this.getAllInterests = function() { return $http.get(srvUrl + url) };
-}]);
